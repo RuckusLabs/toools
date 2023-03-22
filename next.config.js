1f2dc84405f73ee5063bf6f4f22f90b070e8771e
Damian Makki <damian_makki@apple.com>
2023-03-22 10:12:01 -0500

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  basePath: '/gh-pages',
+  reactStrictMode: true,
+  env: {
+    SUPABASEAPIKEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im10ZHpqYWd1anV0emx5cnJmYnVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg5MTE5NTksImV4cCI6MTk5NDQ4Nzk1OX0.hV4C27luxj_o6tfpS2sFt5IUVsYlPvp8Pm5CT0PMFM8',
+    SUPABASEURL: 'https://mtdzjagujutzlyrrfbud.supabase.co',
+  },
+}
